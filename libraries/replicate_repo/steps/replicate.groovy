d3a9ccf7efd11e7525bd384d97c449ca38ecc371
Ever <elux@classvaluation>
2022-06-24 17:27:44 -0600

    starting stage

--- libraries/replicate_repo/steps/replicate.groovy
+++ libraries/replicate_repo/steps/replicate.groovy
@@ -4,4 +4,18 @@ void call(Map params){
     def environment = params.environment ?: 'sandbox'
     def namespace = params.namespace ?: 'develop'
 
+    def repo = ""
+
+    withFolderProperties{
+        repo = env.REPO
+    }
+
+    def app_name = repo.split('/').last()
+
+    node("replicate-repo-${environment}") {
+        stage('Replicate to second region') {
+            sh "echo ${repo}"
+        }
+    }
+
 }
