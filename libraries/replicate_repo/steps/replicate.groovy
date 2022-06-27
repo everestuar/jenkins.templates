36bae3d6ff25a2520132731ea0c21d77655b4c83
Ever <elux@classvaluation>
2022-06-27 13:08:14 -0600

    replicate to second account

--- libraries/replicate_repo/steps/replicate.groovy
+++ libraries/replicate_repo/steps/replicate.groovy
@@ -14,40 +14,45 @@ void call(Map params){
     def named_profile = "to_be_deleted_jenkins"
 
     node("built-in") {
-        stage('Replicate to second region') {
-            sh "echo ${repo}"
-            sh "echo ${repo_name}"
-            sh "pwd"
-            sh "rm -rf LocalRepository"
-            sh "git clone --mirror codecommit::us-east-2://${repo_name} LocalRepository"
-            sh "ls -ltr"
+        try {
+            stage('Replicate to second region') {
+                sh "echo ${repo}"
+                sh "echo ${repo_name}"
+                sh "pwd"
+                sh "rm -rf LocalRepository"
+                sh "git clone --mirror codecommit::us-east-2://${repo_name} LocalRepository"
+                sh "ls -ltr"
 
-            dir("LocalRepository") {
-                sh "ls"
-                sh "git remote set-url --push origin codecommit::us-west-2://${repo_name}"
-                sh "git fetch -p origin"
-                sh "git push --mirror --follow-tags"
+                dir("LocalRepository") {
+                    sh "ls"
+                    sh "git remote set-url --push origin codecommit::us-west-2://${repo_name}"
+                    sh "git fetch -p origin"
+                    sh "git push --mirror --follow-tags"
+                }
+
+                sh "rm -rf LocalRepository"
             }
 
-            sh "rm -rf LocalRepository"
-        }
+            stage("Replicate to second account") {
+                sh "echo ${repo}"
+                sh "echo ${repo_name}"
+                sh "pwd"
+                sh "rm -rf BackupRepository"
+                sh "git clone --mirror codecommit::us-east-2://${repo_name} BackupRepository"
+                sh "ls -ltr"
 
-        stage("Replicate to second account") {
-            sh "echo ${repo}"
-            sh "echo ${repo_name}"
-            sh "pwd"
-            sh "rm -rf BackupRepository"
-            sh "git clone --mirror codecommit::us-east-2://${repo_name} BackupRepository"
-            sh "ls -ltr"
+                dir("BackupRepository") {
+                    sh "ls"
+                    sh "git remote set-url --push origin codecommit::us-east-2://${named_profile}@${repo_name}-bkp"
+                    sh "git fetch -p origin"
+                    sh "git push --mirror --follow-tags"
+                }
 
-            dir("BackupRepository") {
-                sh "ls"
-                sh "git remote set-url --push origin codecommit::us-east-2://${named_profile}@${repo_name}-bkp"
-                sh "git fetch -p origin"
-                sh "git push --mirror --follow-tags"
+                sh "rm -rf BackupRepository"
             }
 
-            sh "rm -rf BackupRepository"
+        } catch (err) {
+            echo "${err}"
         }
     }
 
