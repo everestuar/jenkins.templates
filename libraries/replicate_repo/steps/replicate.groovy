0f3e206d2d472fdf21f379227f74b6581e92df5e
Ever <elux@classvaluation>
2022-06-27 13:04:55 -0600

    replicate to second account

--- libraries/replicate_repo/steps/replicate.groovy
+++ libraries/replicate_repo/steps/replicate.groovy
@@ -11,6 +11,7 @@ void call(Map params){
     }
 
     def repo_name = repo.split('/').last()
+    def named_profile = "to_be_deleted_jenkins"
 
     node("built-in") {
         stage('Replicate to second region') {
@@ -19,21 +20,34 @@ void call(Map params){
             sh "pwd"
             sh "rm -rf LocalRepository"
             sh "git clone --mirror codecommit::us-east-2://${repo_name} LocalRepository"
-            sh "pwd"
             sh "ls -ltr"
 
-            dir("LocalRepository"){
+            dir("LocalRepository") {
                 sh "ls"
                 sh "git remote set-url --push origin codecommit::us-west-2://${repo_name}"
                 sh "git fetch -p origin"
                 sh "git push --mirror --follow-tags"
             }
+
+            sh "rm -rf LocalRepository"
         }
 
         stage("Replicate to second account") {
             sh "echo ${repo}"
             sh "echo ${repo_name}"
             sh "pwd"
+            sh "rm -rf BackupRepository"
+            sh "git clone --mirror codecommit::us-east-2://${repo_name} BackupRepository"
+            sh "ls -ltr"
+
+            dir("BackupRepository") {
+                sh "ls"
+                sh "git remote set-url --push origin codecommit::us-east-2://${named_profile}@${repo_name}-bkp"
+                sh "git fetch -p origin"
+                sh "git push --mirror --follow-tags"
+            }
+
+            sh "rm -rf BackupRepository"
         }
     }
 
